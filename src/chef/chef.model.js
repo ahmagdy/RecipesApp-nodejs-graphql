eacfcc4f5ec5f7f4edc3de401582215145508a4e
Ahmad Magdy <ahmedmagdi@outlook.com>
2018-03-21 22:00:41 +0100

    add chef model

---
+++ src/chef/chef.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const mongooseTypes = mongoose.Schema.Types
+
+const cehfSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 70
+    },
+    position: String,
+    phoneNumber: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+
+export const Chef = mongoose.model('chef', cehfSchema);
